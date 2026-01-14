26b28de2410db0feafebe6f0769bd290163c2c9a
夏目的暗号 <945533645@qq.com>
2026-01-14 15:47:35 +0800

    chore[config]: 给Redis配置类添加@Configuration注解

--- ticketstorm-redis-tool-framework/ticketstorm-redis-common-framework/src/main/java/com/ticketstorm/config/RedisFrameWorkAutoConfig.java
+++ ticketstorm-redis-tool-framework/ticketstorm-redis-common-framework/src/main/java/com/ticketstorm/config/RedisFrameWorkAutoConfig.java
@@ -2,13 +2,14 @@ package com.ticketstorm.config;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-
+@Configuration
 public class RedisFrameWorkAutoConfig {
 
     @Bean("redisToolRedisTemplate")
