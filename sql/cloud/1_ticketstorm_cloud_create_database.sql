fbb6a9665a2cd07bdf8dbe26e584a1f15e790784
夏目的暗号 <945533645@qq.com>
2026-01-24 10:27:38 +0800

    chore: 新增数据库的创建数据库、创建表和数据SQL文件

---
+++ sql/cloud/1_ticketstorm_cloud_create_database.sql
@@ -0,0 +1,11 @@
+-- 最先执行这个，来创建数据库
+create database if not exists ticketstorm_base_data character set utf8mb4;
+create database if not exists ticketstorm_customize character set utf8mb4;
+create database if not exists ticketstorm_order_0 character set utf8mb4;
+create database if not exists ticketstorm_order_1 character set utf8mb4;
+create database if not exists ticketstorm_pay_0 character set utf8mb4;
+create database if not exists ticketstorm_pay_1 character set utf8mb4;
+create database if not exists ticketstorm_program_0 character set utf8mb4;
+create database if not exists ticketstorm_program_1 character set utf8mb4;
+create database if not exists ticketstorm_user_0 character set utf8mb4;
+create database if not exists ticketstorm_user_1 character set utf8mb4;
